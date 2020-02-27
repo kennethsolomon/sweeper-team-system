9f7a1edbf451498815bdaa0c696b5ffe7b17bd72
Kenneth Solomon <synecders@gmail.com>
2020-02-27 17:23:01 +0800

    update database

--- database/dietary.sql
+++ database/dietary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2020 at 09:21 AM
+-- Generation Time: Feb 27, 2020 at 10:22 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -52,7 +52,19 @@ INSERT INTO `patient` (`id`, `uId`, `lastName`, `firstName`, `middleName`, `date
 (30, 'k74gqrdqyllhbq7p3ni', 'Solomon', 'Mark', 'Lim', '1997-07-12', NULL, '2020-02-27 16:05:45.392913', 'Pedia/Surgery', '', '', ''),
 (31, 'k74gqrdqyllhbq7p3ni', 'Huenda', 'Aldrin', 'Lim', '1997-07-12', NULL, '2020-02-27 16:05:45.392913', 'Pedia/Surgery', '', '', ''),
 (32, 'k74h2tqeebrz3g4plvo', 'te', '', '', '0000-00-00', NULL, '2020-02-27 16:15:08.311929', 'General', '', '', ''),
-(33, 'k74h8zparvy5wft1xdg', 'Solomon', 'Kenneth', 'Lim', '1997-07-12', NULL, '2020-02-27 16:19:55.985122', 'OB', '', '', '');
+(33, 'k74h8zparvy5wft1xdg', 'Solomon', 'Kenneth', 'Lim', '1997-07-12', NULL, '2020-02-27 16:19:55.985122', 'OB', '', '', ''),
+(34, 'k74hzt68j8iv4ola68', 'test', 'test', 'test', '0000-00-00', NULL, '2020-02-27 16:40:47.224760', 'General', '', '', ''),
+(35, 'k74ibsdi6mzobuhfdmy', 'Solomon', 'Kenneth', 'Lim', '1998-07-12', NULL, '2020-02-27 16:50:06.070600', 'General', '', '', ''),
+(37, 'k74iep53z1i0rb14pf9', '', '', '', '0000-00-00', NULL, '2020-02-27 16:52:21.846632', 'General', '', '', ''),
+(38, 'k74ikkw1yqfnh1lwo4', 's', '', '', '0000-00-00', NULL, '2020-02-27 16:56:56.275398', 'General', '', '', ''),
+(39, 'k74im43sessem0zbnbl', 'a', '', '', '0000-00-00', NULL, '2020-02-27 16:58:07.832012', '', '', '', ''),
+(40, 'k74it85lguk1oent8kg', 'ssssss', '', '', '0000-00-00', NULL, '2020-02-27 17:03:39.674843', 'General', '', '', ''),
+(41, 'k74j7akxe0g0163ien', 'ss', 's', 's', '0023-12-31', NULL, '2020-02-27 17:14:36.005045', 'General', '', '', ''),
+(42, 'k74j7tjn97mtuwhcfj8', 's', 's', 's', '0123-12-31', NULL, '2020-02-27 17:15:00.584783', 'General', '', '', ''),
+(43, 'k74j7ydln5bqio2k80h', 'qwe', 'qwe', 'ewq', '0023-12-31', NULL, '2020-02-27 17:15:06.843602', '', '', '', ''),
+(44, 'k74j8n4xtit2bxg6r0h', 's', 's', 's', '0023-12-31', NULL, '2020-02-27 17:15:38.934596', 'General', '', '', ''),
+(45, 'k74j8w96x34oxk6a89f', '123', '123', '123', '0132-12-31', NULL, '2020-02-27 17:15:50.747571', '', '', '', ''),
+(46, 'k74j9hwxkn8eh92i95g', 'test', 'test', 'test', '0023-12-31', NULL, '2020-02-27 17:16:18.819210', 'OB', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +84,7 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

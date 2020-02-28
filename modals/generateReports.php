db2043179226fd234d9c06190a2a71bfcfcf4c9d
Kenneth Solomon <synecders@gmail.com>
2020-02-28 11:20:40 +0800

    add generate reports

--- modals/generateReports.php
+++ modals/generateReports.php
@@ -4,63 +4,40 @@
         <div class="modal-content">
             <div class="modal-header justify-content-center">
                 <div class="modal-profile">
-                    <i class="nc-icon nc-circle-09"></i>
+                    <i class="nc-icon nc-paper-2"></i>
                 </div>
             </div>
             <div class="modal-body text-center">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">Add Patient</h4>
+                        <h4 class="card-title">Date Range</h4>
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-12 pl-1">
-                                <div class="form-group">
-                                    <input id="lastName" name="lastName" type="text" class="form-control" placeholder="Last Name" value="">
+                    <form action="report.php" method="get">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12 pl-1">
+                                    <div class="form-group">
+                                        <input id="pId" name="pId" type="hidden" class="form-control" value="<?php echo $_GET['uId'] ?>">
+                                    </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6 pl-1">
+                                    <div class="form-group">
+                                        <input id="dateOfBirth" name="dateOfBirth" type="date" class="form-control" placeholder="Last Name" value="">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 pl-1">
+                                    <div class="form-group">
+                                        <input id="dateOfBirth" name="dateOfBirth" type="date" class="form-control" placeholder="Last Name" value="">
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" name="generateReportBtn" class="btn btn-info btn-fill pull-right ml-3">Generate</button>
+                            <div class="clearfix"></div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12 pl-1">
-                                <div class="form-group">
-                                    <input id="firstName" name="firstName" type="text" class="form-control" placeholder="First Name" value="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12 pl-1">
-                                <div class="form-group">
-                                    <input id="middleName" name="middleName" type="text" class="form-control" placeholder="Middle Name" value="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12 pl-1">
-                                <div class="form-group">
-                                    <input id="dateOfBirth" name="dateOfBirth" type="date" class="form-control" placeholder="Last Name" value="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 pr-1">
-                                <div class="form-group">
-                                    <label>Wards</label>
-                                    <select id="ward" class="form-control">
-                                        <option value="General">General</option>
-                                        <option value="Pedia/Surgery">Pedia/Surgery</option>
-                                        <option value="OB">OB</option>
-                                        <option value="Rehy/ISO">Rehy/ISO</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 pl-1 mt-5">
-                                <div class="form-group">
-                                    <button type="submit" id="saveBtn" class="btn btn-info btn-fill pull-right">Save Patient</button>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
+
                 </div>
             </div>
             <div class="modal-footer">

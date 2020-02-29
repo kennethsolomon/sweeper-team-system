61506e991820142120c29fd534bf9f9b23513194
Kenneth Solomon <synecders@gmail.com>
2020-02-29 19:11:53 +0800

    update database

--- database/dietary.sql
+++ database/dietary.sql
@@ -2,9 +2,9 @@
 -- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 29, 2020 at 06:30 AM
--- Server version: 10.4.11-MariaDB
+-- Host: localhost
+-- Generation Time: Feb 29, 2020 at 11:11 AM
+-- Server version: 10.4.12-MariaDB
 -- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -36,47 +36,16 @@ CREATE TABLE `patient` (
   `middleName` varchar(255) NOT NULL,
   `dateOfBirth` date DEFAULT NULL,
   `createdAt` datetime(6) DEFAULT current_timestamp(6),
-  `ward` varchar(255) NOT NULL,
-  `day01` varchar(255) NOT NULL,
-  `day02` varchar(255) NOT NULL,
-  `day03` varchar(255) NOT NULL,
-  `day04` varchar(255) NOT NULL,
-  `day05` varchar(255) NOT NULL,
-  `day06` varchar(255) NOT NULL,
-  `day07` varchar(255) NOT NULL,
-  `day08` varchar(255) NOT NULL,
-  `day09` varchar(255) NOT NULL,
-  `day10` varchar(255) NOT NULL,
-  `day11` varchar(255) NOT NULL,
-  `day12` varchar(255) NOT NULL,
-  `day13` varchar(255) NOT NULL,
-  `day14` varchar(255) NOT NULL,
-  `day15` varchar(255) NOT NULL,
-  `day16` varchar(255) NOT NULL,
-  `day17` varchar(255) NOT NULL,
-  `day18` varchar(255) NOT NULL,
-  `day19` varchar(255) NOT NULL,
-  `day20` varchar(255) NOT NULL,
-  `day21` varchar(255) NOT NULL,
-  `day22` varchar(255) NOT NULL,
-  `day23` varchar(255) NOT NULL,
-  `day24` varchar(255) NOT NULL,
-  `day25` varchar(255) NOT NULL,
-  `day26` varchar(255) NOT NULL,
-  `day27` varchar(255) NOT NULL,
-  `day28` varchar(255) NOT NULL,
-  `day29` varchar(255) NOT NULL,
-  `day30` varchar(255) NOT NULL,
-  `day31` varchar(255) NOT NULL
+  `ward` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`id`, `uId`, `lastName`, `firstName`, `middleName`, `dateOfBirth`, `createdAt`, `ward`, `day01`, `day02`, `day03`, `day04`, `day05`, `day06`, `day07`, `day08`, `day09`, `day10`, `day11`, `day12`, `day13`, `day14`, `day15`, `day16`, `day17`, `day18`, `day19`, `day20`, `day21`, `day22`, `day23`, `day24`, `day25`, `day26`, `day27`, `day28`, `day29`, `day30`, `day31`) VALUES
-(51, 'k75j52f63675bsbjxsh', 'Solomon', 'Kenneth', 'Lim', '1997-07-12', '2020-02-28 10:00:38.292321', 'General', 'l', '', '', '', '', '', '', '', '', '', '', 'bd', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bl', 'bld', '', ''),
-(53, 'k76xpkcxwm97n8ms61', 'Huenda', 'Aldrin Ray', 'Borja', '1997-07-13', '2020-02-29 09:36:15.463853', 'OB', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bl', '', '');
+INSERT INTO `patient` (`id`, `uId`, `lastName`, `firstName`, `middleName`, `dateOfBirth`, `createdAt`, `ward`) VALUES
+(51, 'k75j52f63675bsbjxsh', 'Solomon', 'Kenneth', 'Lim', '1997-07-12', '2020-02-28 10:00:38.292321', 'General'),
+(53, 'k76xpkcxwm97n8ms61', 'Huenda', 'Aldrin Ray', 'Borja', '1997-07-13', '2020-02-29 09:36:15.463853', 'OB');
 
 -- --------------------------------------------------------
 
@@ -92,50 +61,65 @@ CREATE TABLE `patientsubsistence` (
   `date` date NOT NULL,
   `breakfast` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lunch` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dinner` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `day01` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day02` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day03` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day04` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day05` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day06` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day07` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day08` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day09` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day10` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day11` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day12` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day13` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day14` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day15` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day16` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day17` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day18` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day19` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day20` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day21` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day22` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day23` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day24` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day25` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day26` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day27` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day28` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day29` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day30` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `day31` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `dinner` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `patientsubsistence`
 --
 
-INSERT INTO `patientsubsistence` (`id`, `pId`, `month`, `year`, `date`, `breakfast`, `lunch`, `dinner`, `day01`, `day02`, `day03`, `day04`, `day05`, `day06`, `day07`, `day08`, `day09`, `day10`, `day11`, `day12`, `day13`, `day14`, `day15`, `day16`, `day17`, `day18`, `day19`, `day20`, `day21`, `day22`, `day23`, `day24`, `day25`, `day26`, `day27`, `day28`, `day29`, `day30`, `day31`) VALUES
-(58, 'k75j52f63675bsbjxsh', NULL, NULL, '2020-02-29', 'on', 'on', 'on', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'dl', NULL, NULL),
-(59, 'k75j52f63675bsbjxsh', NULL, NULL, '2020-02-28', 'on', 'on', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'bl', NULL, NULL, NULL),
-(60, 'k76xpkcxwm97n8ms61', NULL, NULL, '2020-02-29', 'on', 'on', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(61, 'k75j52f63675bsbjxsh', NULL, NULL, '2020-02-01', '', 'on', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(62, 'k75j52f63675bsbjxsh', NULL, NULL, '2020-02-12', 'on', '', 'on', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `patientsubsistence` (`id`, `pId`, `month`, `year`, `date`, `breakfast`, `lunch`, `dinner`) VALUES
+(83, 'k76xpkcxwm97n8ms61', NULL, NULL, '2020-02-29', 'on', 'on', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(12) NOT NULL,
+  `pId` varchar(25) NOT NULL,
+  `date` date NOT NULL,
+  `day01` varchar(255) DEFAULT NULL,
+  `day02` varchar(255) DEFAULT NULL,
+  `day03` varchar(255) DEFAULT NULL,
+  `day04` varchar(255) DEFAULT NULL,
+  `day05` varchar(255) DEFAULT NULL,
+  `day06` varchar(255) DEFAULT NULL,
+  `day07` varchar(255) DEFAULT NULL,
+  `day08` varchar(255) DEFAULT NULL,
+  `day09` varchar(255) DEFAULT NULL,
+  `day10` varchar(255) DEFAULT NULL,
+  `day11` varchar(255) DEFAULT NULL,
+  `day12` varchar(255) DEFAULT NULL,
+  `day13` varchar(255) DEFAULT NULL,
+  `day14` varchar(255) DEFAULT NULL,
+  `day15` varchar(255) DEFAULT NULL,
+  `day16` varchar(255) DEFAULT NULL,
+  `day17` varchar(255) DEFAULT NULL,
+  `day18` varchar(255) DEFAULT NULL,
+  `day19` varchar(255) DEFAULT NULL,
+  `day20` varchar(255) DEFAULT NULL,
+  `day21` varchar(255) DEFAULT NULL,
+  `day22` varchar(255) DEFAULT NULL,
+  `day23` varchar(255) DEFAULT NULL,
+  `day24` varchar(255) DEFAULT NULL,
+  `day25` varchar(255) DEFAULT NULL,
+  `day26` varchar(255) DEFAULT NULL,
+  `day27` varchar(255) DEFAULT NULL,
+  `day28` varchar(255) DEFAULT NULL,
+  `day29` varchar(255) DEFAULT NULL,
+  `day30` varchar(255) DEFAULT NULL,
+  `day31` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`id`, `pId`, `date`, `day01`, `day02`, `day03`, `day04`, `day05`, `day06`, `day07`, `day08`, `day09`, `day10`, `day11`, `day12`, `day13`, `day14`, `day15`, `day16`, `day17`, `day18`, `day19`, `day20`, `day21`, `day22`, `day23`, `day24`, `day25`, `day26`, `day27`, `day28`, `day29`, `day30`, `day31`) VALUES
+(57, 'k76xpkcxwm97n8ms61', '2020-02-29', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'bl', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -154,6 +138,12 @@ ALTER TABLE `patientsubsistence`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -167,7 +157,13 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `patientsubsistence`
 --
 ALTER TABLE `patientsubsistence`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

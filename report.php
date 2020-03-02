0dadae9c5b110f0cd5eb43de3ba196fdc09a585b
Kenneth Solomon <synecders@gmail.com>
2020-03-02 10:21:38 +0800

    fix notice array offset error

--- report.php
+++ report.php
@@ -124,9 +124,9 @@
                                     $day31 = $row['day31'];
 
                                     // TOTAL FORMULA
-                                    $listOfDayArray = array($day01, $day02, $day03, $day04, $day05, $day06, $day07, $day08, $day09, $day10, $day11, $day12, $day13, $day14, $day15, $day16, $day17, $day18, $day19, $day20, $day21, $day22, $day23, $day24, $day25, $day25, $day27, $day28, $day29, $day30, $day31,);
+                                    $listOfDayArray = array($day01, $day02, $day03, $day04, $day05, $day06, $day07, $day08, $day09, $day10, $day11, $day12, $day13, $day14, $day15, $day16, $day17, $day18, $day19, $day20, $day21, $day22, $day23, $day24, $day25, $day25, $day27, $day28, $day29, $day30, $day31);
                                     $countDay = 0;
-                                    for ($listOfDay = 0; count($listOfDayArray) >= $listOfDay; $listOfDay++) {
+                                    for ($listOfDay = 0; count($listOfDayArray) > $listOfDay; $listOfDay++) {
                                         $countDay = $countDay + strlen($listOfDayArray[$listOfDay]);
                                     }
 
